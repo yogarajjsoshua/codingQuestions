bbd759b79069bd66f83bada094537c83d4eeb982
yogaraj <yogaraj.j@simplify3x.com>
2026-04-06 07:33:34 +0530

    making Changes

--- flaskApp.py
+++ flaskApp.py
@@ -1,17 +1,25 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from huggingface_hub import login
 from pydantic import BaseModel
 
+load_dotenv()
+
+
 class users(BaseModel):
-    id:int
-    usr_name:str
-    usr_age:int
+    id: int
+    usr_name: str
+    usr_age: int
+
 
 app = FastAPI()
 
-app.post("/users")
-def create_user(user:users):
-    return {"message":"User created successfully"}
 
-app.get("/users/{id}")
-def get_users(id:int):
-    return {"message":"Users fetched successfully", "id":id}
+@app.post("/users")
+def create_user(user: users):
+    return {"message": "User created successfully"}
+
+
+@app.get("/users/{id}")
+def get_users(id: int):
+    return {"message": "Users fetched successfully", "id": id}
